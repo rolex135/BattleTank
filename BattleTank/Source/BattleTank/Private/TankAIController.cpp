2f07a92f09127bc3b1d380ffb959953512093596
MARTIN-PC\Martin <martin.k6re@gmail.com>
2016-12-08 10:44:50 +0200

    ai can find playercontroller

--- BattleTank/Source/BattleTank/Private/TankAIController.cpp
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -1,7 +1,6 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "BattleTank.h"
-#include "TankAIController.h"
 #include "../Public/TankAIController.h"
 
 
@@ -9,18 +8,27 @@
 void ATankAIController::BeginPlay()
 {
 	Super::BeginPlay();
-	auto ControlledTank = GetAIControlledTank();
-	if (!ControlledTank)
+
+	auto PlayerTank = GetPlayerTank();
+	if(!PlayerTank)
 	{
-		UE_LOG(LogTemp, Warning, TEXT("No Controlled Tank"));
+		UE_LOG(LogTemp, Warning, TEXT("AIController can't find player tank"));
 	}
 	else
 	{
-		UE_LOG(LogTemp, Warning, TEXT("AIController possesing: %s"), *ControlledTank->GetName());
+		UE_LOG(LogTemp, Warning, TEXT("AIController found player: %s"), *PlayerTank->GetName());
 	}
 }
 
-ATank * ATankAIController::GetAIControlledTank() const
+ATank* ATankAIController::GetAIControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+ATank* ATankAIController::GetPlayerTank() const
+{
+	auto PlayerPawn = GetWorld()->GetFirstPlayerController()->GetPawn();
+	if (!PlayerPawn) { return nullptr; }
+
+	return Cast<ATank>(PlayerPawn);
 }
--- BattleTank/Source/BattleTank/Public/TankAIController.h
+++ BattleTank/Source/BattleTank/Public/TankAIController.h
@@ -14,9 +14,10 @@ class BATTLETANK_API ATankAIController : public AAIController
 {
 	GENERATED_BODY()
 	
-
-public:
+private:
 	ATank* GetAIControlledTank() const;
 
 	virtual void BeginPlay() override;
+
+	ATank* GetPlayerTank() const;
 };
