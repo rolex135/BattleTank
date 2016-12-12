cb09cdbdba026cab4dedfd97171a17361ea9dc2e
MARTIN-PC\Martin <martin.k6re@gmail.com>
2016-12-12 09:33:18 +0200

    barrel moves up and down, regarding where to aim

--- BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
+++ BattleTank/Source/BattleTank/Private/TankAimingComponent.cpp
@@ -57,5 +57,5 @@ void UTankAimingComponent::MoveBarrelTowards(FVector AimDirection)
 	auto BarrelRotator = Barrel->GetForwardVector().Rotation();
 	auto AimAsRotator = AimDirection.Rotation();
 	auto DeltaRotator = AimAsRotator - BarrelRotator;
-	Barrel->Elevate(5.f);//TODO remove magic number
+	Barrel->Elevate(DeltaRotator.Pitch);//TODO remove magic number
 }
