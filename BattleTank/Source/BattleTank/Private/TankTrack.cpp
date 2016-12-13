4884730f8184c864adfef0065df3cb1b0447de5f
MARTIN-PC\Martin <martin.k6re@gmail.com>
2016-12-13 10:58:22 +0200

    individual track binding for keyboard and gamepad

---
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTank.h"
+#include "TankTrack.h"
+
+
+void UTankTrack::SetThrottle(float Throttle)
+{
+	auto Name = GetName();
+	UE_LOG(LogTemp, Warning, TEXT("%s throttle, %f"), *Name, Throttle);
+	
+	//TODO clamp actual throttle value so player cant over-drive
+}
+
+
