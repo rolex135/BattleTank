3b57e8989e00049a48dbd64ab93c7b125945231f
MARTIN-PC\Martin <martin.k6re@gmail.com>
2016-12-13 12:51:43 +0200

    tank throttleing and tank movement component

--- BattleTank/Source/BattleTank/Private/TankTrack.cpp
+++ BattleTank/Source/BattleTank/Private/TankTrack.cpp
@@ -6,10 +6,15 @@
 
 void UTankTrack::SetThrottle(float Throttle)
 {
+	//Throttle = FMath::Clamp<float>(Throttle, -1, 1);
 	auto Name = GetName();
 	UE_LOG(LogTemp, Warning, TEXT("%s throttle, %f"), *Name, Throttle);
 	
 	//TODO clamp actual throttle value so player cant over-drive
+	auto ForceApplied = GetForwardVector() * Throttle * TrackMaxDrivingForce;
+	auto ForceLocation = GetComponentLocation();
+	auto TankRoot = Cast<UPrimitiveComponent>(GetOwner()->GetRootComponent());
+	TankRoot->AddForceAtLocation(ForceApplied, ForceLocation);
 }
 
 
