325c68b6cbc6bf291bdcb149000a88ebe4a7e4f8
MARTIN-PC\Martin <martin.k6re@gmail.com>
2016-12-23 09:45:51 +0200

    tanks have health bar

--- BattleTank/Source/BattleTank/Private/Tank.cpp
+++ BattleTank/Source/BattleTank/Private/Tank.cpp
@@ -23,3 +23,8 @@ float ATank::TakeDamage(float DamageAmount, struct FDamageEvent const & DamageEv
 	}
 	return DamageToApply;
 }
+
+float ATank::GetHealthPercent() const
+{
+	return (float)CurrentHealth / (float)StartingHealth;
+}
--- BattleTank/Source/BattleTank/Public/Tank.h
+++ BattleTank/Source/BattleTank/Public/Tank.h
@@ -12,12 +12,17 @@ class BATTLETANK_API ATank : public APawn
 
 
 public:
+	//called by the engine when actor damage is dealt
 	virtual float TakeDamage(float DamageAmount,struct FDamageEvent const & DamageEvent,
 		class AController * EventInstigator, AActor * DamageCauser) override;
 
-	ATank();
+	//returns current health as a percentage of starting health, between 0 and 1
+	UFUNCTION(BlueprintPure, Category = "Health")
+	float GetHealthPercent() const;
 
 private:
+	ATank();
+
 	UPROPERTY(EditDefaultsOnly, Category = "Setup")
 	int32 StartingHealth = 100;
 
